5fc0b87ae3603f3f2b949051d8a8e80004971b69
lamaxiyali <3293376887@qq.com>
2021-01-14 15:19:12 +0800

    first commit

--- src/main/java/Demo3.java
+++ src/main/java/Demo3.java
@@ -2,5 +2,6 @@ public class Demo3 {
     public static void main(String[] args) {
         System.out.println("demo3");
         System.out.println("second demo3");
+        System.out.println("thrid demo3");
     }
 }
